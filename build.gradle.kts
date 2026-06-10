a798cce828d3ea89c486abc3a086fd764fc26e70
ydbb <yeondububub@gmail.com>
2026-06-10 14:42:34 +0900

    setting project - feat: add docker-compose for mysql and redis - chore: add backend dependencies for jpa, redis and mysql - feat: add database and redis configuration in application.yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,9 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    runtimeOnly("com.mysql:mysql-connector-j")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
